4341375ae913ecb7a9bd612e5cd49f1bae730434
slujan <slujan@ratherlabs.com>
2025-08-13 04:38:15 -0300

    feat: add hero component

---
+++ frontend/omnipay-app/components/hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <>
+            <main className="container mx-auto px-4 py-8 relative z-10">
+                Hero Component
+            </main>
+        </>
+    )
+}
