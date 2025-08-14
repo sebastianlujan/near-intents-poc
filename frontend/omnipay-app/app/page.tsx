cea7f2c630043f19e9317c0df6cce79ee6d839d0
slujan <slujan@ratherlabs.com>
2025-08-14 14:05:58 -0300

    feat: add hero section

--- frontend/omnipay-app/app/page.tsx
+++ frontend/omnipay-app/app/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import { useState } from "react"
 import { Navigation } from "@/components/navigation"
+import { Hero } from "@/components/hero"
+
 
 export default function OmnipayLandingPage() {
   const [activeTab, setActiveTab] = useState<"buyer" | "seller">("seller")
@@ -9,8 +11,10 @@ export default function OmnipayLandingPage() {
     <div className="min-h-screen text-white flex flex-col bg-gradient-radial from-[#23203E] via-[#23203E] to-[#0C0C17]">
 
       <Navigation />
+      {/* Main content */}
       <div className="flex-1 flex items-center justify-center px-8">
         <div className="flex flex-col justify-center items-center gap-8 w-[800px] h-[662px] flex-none">
+          <Hero />
         </div>
       </div>
     </div>
