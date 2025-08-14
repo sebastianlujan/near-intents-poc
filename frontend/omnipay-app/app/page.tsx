056a24e7c5c7294e4efadd770c4c031126d11dc3
slujan <slujan@ratherlabs.com>
2025-08-14 13:49:39 -0300

    feat: add navbar

--- frontend/omnipay-app/app/page.tsx
+++ frontend/omnipay-app/app/page.tsx
@@ -1,11 +1,14 @@
 "use client"
 import { useState } from "react"
+import { Navigation } from "@/components/navigation"
 
 export default function OmnipayLandingPage() {
   const [activeTab, setActiveTab] = useState<"buyer" | "seller">("seller")
 
   return (
     <div className="min-h-screen text-white flex flex-col bg-gradient-radial from-[#23203E] via-[#23203E] to-[#0C0C17]">
+
+      <Navigation />
       <div className="flex-1 flex items-center justify-center px-8">
         <div className="flex flex-col justify-center items-center gap-8 w-[800px] h-[662px] flex-none">
         </div>
--- frontend/omnipay-app/components/navigation.tsx
+++ frontend/omnipay-app/components/navigation.tsx
@@ -1,15 +1,24 @@
 "use client"
 
-import { Main } from "next/document"
-import { useState } from "react"
+import Image from "next/image";
 
-export default function LandingPage() {
-
+export function Navigation() {
     return (
-        <>
-            <Header/>
-            <MainContent/>
-            <Footer/>
-        </>
+        <nav className="w-full">
+            <div className="max-w-7xl mx-auto px-6 py-4">
+                <div className="flex items-center justify-between">
+                    { /* Logo and title */ }
+                    <div className="flex items-center gap-4">
+                        <div className="w-11 h-11 flex items-center justify-center">
+                            <Image src="/logo.svg" alt="OmniPay Logo" width={44} height={44} />
+                        </div>
+                        <div className="w-0.5 h-11 bg-white/20"></div>
+                         <span className="font-electrolize text-2xl font-normal tracking-[0.15em] uppercase text-white">
+                            OMNIPAY
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
